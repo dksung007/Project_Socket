f3bc44b6449461c33412314cb3ff3b182962871e
Ekkaphol <ekkaphol.loo@outlook.com>
2017-06-10 22:59:34 +0700

    update

--- Socket_Receive/UI/UI_Receive.Designer.vb
+++ Socket_Receive/UI/UI_Receive.Designer.vb
@@ -22,17 +22,141 @@ Partial Class UI_Receive
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(UI_Receive))
+        Me.AxWinsock1 = New AxMSWinsockLib.AxWinsock()
+        Me.lbl_status = New System.Windows.Forms.Label()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.LB_Log = New System.Windows.Forms.ListBox()
+        Me.lbl_request_connect = New System.Windows.Forms.Label()
+        Me.LB_Data = New System.Windows.Forms.ListBox()
+        CType(Me.AxWinsock1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'AxWinsock1
+        '
+        Me.AxWinsock1.Enabled = True
+        Me.AxWinsock1.Location = New System.Drawing.Point(491, 12)
+        Me.AxWinsock1.Name = "AxWinsock1"
+        Me.AxWinsock1.OcxState = CType(resources.GetObject("AxWinsock1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWinsock1.Size = New System.Drawing.Size(28, 28)
+        Me.AxWinsock1.TabIndex = 0
+        '
+        'lbl_status
+        '
+        Me.lbl_status.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lbl_status.AutoSize = True
+        Me.lbl_status.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lbl_status.Location = New System.Drawing.Point(12, 250)
+        Me.lbl_status.Name = "lbl_status"
+        Me.lbl_status.Size = New System.Drawing.Size(73, 25)
+        Me.lbl_status.TabIndex = 1
+        Me.lbl_status.Text = "Status"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 37)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(507, 210)
+        Me.TabControl1.TabIndex = 2
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.LB_Data)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(499, 184)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Data"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.LB_Log)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(499, 184)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Log"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'LB_Log
+        '
+        Me.LB_Log.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LB_Log.BackColor = System.Drawing.Color.Black
+        Me.LB_Log.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.LB_Log.ForeColor = System.Drawing.Color.Lime
+        Me.LB_Log.FormattingEnabled = True
+        Me.LB_Log.ItemHeight = 16
+        Me.LB_Log.Location = New System.Drawing.Point(6, 6)
+        Me.LB_Log.Name = "LB_Log"
+        Me.LB_Log.Size = New System.Drawing.Size(487, 164)
+        Me.LB_Log.TabIndex = 0
+        '
+        'lbl_request_connect
+        '
+        Me.lbl_request_connect.AutoSize = True
+        Me.lbl_request_connect.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lbl_request_connect.Location = New System.Drawing.Point(12, 9)
+        Me.lbl_request_connect.Name = "lbl_request_connect"
+        Me.lbl_request_connect.Size = New System.Drawing.Size(207, 25)
+        Me.lbl_request_connect.TabIndex = 3
+        Me.lbl_request_connect.Text = "Request Connection"
+        '
+        'LB_Data
+        '
+        Me.LB_Data.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LB_Data.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.LB_Data.FormattingEnabled = True
+        Me.LB_Data.ItemHeight = 16
+        Me.LB_Data.Location = New System.Drawing.Point(6, 6)
+        Me.LB_Data.Name = "LB_Data"
+        Me.LB_Data.Size = New System.Drawing.Size(487, 164)
+        Me.LB_Data.TabIndex = 0
         '
         'UI_Receive
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(531, 248)
+        Me.ClientSize = New System.Drawing.Size(531, 284)
+        Me.Controls.Add(Me.lbl_request_connect)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.lbl_status)
+        Me.Controls.Add(Me.AxWinsock1)
         Me.Name = "UI_Receive"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "UI_Receive"
+        CType(Me.AxWinsock1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents AxWinsock1 As AxMSWinsockLib.AxWinsock
+    Friend WithEvents lbl_status As Label
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents lbl_request_connect As Label
+    Friend WithEvents LB_Log As ListBox
+    Friend WithEvents LB_Data As ListBox
 End Class
--- Socket_Receive/UI/UI_Receive.vb
+++ Socket_Receive/UI/UI_Receive.vb
@@ -1,3 +1,55 @@
 ﻿Public Class UI_Receive
+    Private _port As Integer = 9100
+    Private iSockets As Integer = 0
+    Private sRequestID As String
+
+    Private Sub UI_Receive_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        AxWinsock1.LocalPort = _port
+        Text = AxWinsock1.LocalIP & ":" & AxWinsock1.LocalPort
+        KeyPreview = True
+        add_log("Start to Port Listening : " & AxWinsock1.LocalPort)
+        AxWinsock1.Listen()
+
+    End Sub
+
+    Private Sub AxWinsock1_DataArrival(sender As Object, e As AxMSWinsockLib.DMSWinsockControlEvents_DataArrivalEvent) Handles AxWinsock1.DataArrival
+        add_log(" Received: Data from " & AxWinsock1().RemoteHostIP & "(" & sRequestID & ")")
+        Dim strData As Object = vbNullString
+        Dim i As Integer
+        Dim tstring As String = String.Empty
+        AxWinsock1.GetData(strData)
+        For i = 0 To UBound(strData)
+            tstring = tstring + Chr(strData(i))
+        Next
+        LB_Data.Items.Add(Now & tstring)
+        If LB_Data.Items.Count > 0 Then LB_Data.SelectedIndex = LB_Data.Items.Count - 1
+
+    End Sub
+
+    Private Sub AxWinsock1_CloseEvent(sender As Object, e As EventArgs) Handles AxWinsock1.CloseEvent
+        lbl_status.Text = "Status : " + "Disconnect Server"
+        add_log(lbl_status.Text)
+    End Sub
+
+    Private Sub AxWinsock1_ConnectionRequest(sender As Object, e As AxMSWinsockLib.DMSWinsockControlEvents_ConnectionRequestEvent) Handles AxWinsock1.ConnectionRequest
+        If AxWinsock1.CtlState <> MSWinsockLib.StateConstants.sckConnected Then
+            add_log("Connection request id " & e.requestID & " from " & AxWinsock1().RemoteHostIP)
+            sRequestID = e.requestID
+            iSockets = iSockets + 1
+            AxWinsock1.Close()
+            lbl_status.Text = "Status : " + "Connected Server"
+            AxWinsock1.LocalPort = _port
+            AxWinsock1.Accept(e.requestID)
+            lbl_request_connect.Text = "Connection Request : " & iSockets & " Time"
+            add_log(lbl_status.Text)
+        End If
+    End Sub
+
+    Private Sub add_log(ByVal _msg As String)
+        LB_Log.Items.Add(Now & " " & _msg)
+        If LB_Log.Items.Count > 0 Then _
+             LB_Log.SelectedIndex = LB_Log.Items.Count - 1
+    End Sub
 
 End Class
